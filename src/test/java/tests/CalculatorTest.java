cb9fd307a8fdd49aa907c53af67972fb571d2691
ravindrakhote <ravindrakhote@gmail.com>
2023-08-01 16:01:37 +0530

    First Commit

---
+++ src/test/java/tests/CalculatorTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import app.Calculator;
+
+public class CalculatorTest {
+
+	@Test
+	public void testAdd() {
+		int exp = 30;
+		Calculator cal = new Calculator();
+		int act = cal.add(10, 20);
+		assertEquals(exp, act);
+		
+		
+	}
+
+}
